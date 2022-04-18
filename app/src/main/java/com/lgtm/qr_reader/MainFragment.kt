7ee79484f8c2c5e1dc8070980d26d95d4114c491
tofhddl9 <tofhddl9@gmail.com>
2022-04-18 21:51:06 +0900

    Add qrcode scanner

--- app/src/main/java/com/lgtm/qr_reader/MainFragment.kt
+++ app/src/main/java/com/lgtm/qr_reader/MainFragment.kt
@@ -1,18 +1,39 @@
 package com.lgtm.qr_reader
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.lgtm.qr_reader.databinding.FragmentMainBinding
 import com.lgtm.qr_reader.delegate.viewBinding
 
+import com.journeyapps.barcodescanner.BarcodeCallback
+
 class MainFragment: Fragment(R.layout.fragment_main) {
 
     private val binding by viewBinding(FragmentMainBinding::bind)
 
+    private val callback: BarcodeCallback = BarcodeCallback { result ->
+        if (result.text != null) {
+            Log.d("Doran", "$result")
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView.text = "main"
+
+        binding.qrcodeView.decodeContinuous(callback)
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.qrcodeView.resume()
+    }
+
+    override fun onPause() {
+        binding.qrcodeView.pause()
+        super.onPause()
+    }
+
 
 }
